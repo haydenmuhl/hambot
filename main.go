94b544275870aa6a7ff819adf981bbc38be6a815
Hayden Muhl <haydenmuhl@gmail.com>
2018-11-02 21:19:25 -0700

    Add a barebones admin web interface
    Prototype of turning a chat bot on from a web interface.

--- main.go
+++ main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/haydenmuhl/hambot/chat"
 )
 
 func main() {
-	err := chat.Client().Run()
+	var err error
+	mux := http.NewServeMux()
 
+	rootHandler := http.HandlerFunc(func(r http.ResponseWriter, req *http.Request) {
+		if req.URL.String() != "/" {
+			r.WriteHeader(http.StatusNotFound)
+			r.Write([]byte("404 Not Found"))
+			return
+		}
+		r.WriteHeader(http.StatusOK)
+		r.Write([]byte(`<a href="/on">Turn on</a>`))
+		return
+	})
+	mux.Handle("/", rootHandler)
+
+	onHandler := http.HandlerFunc(func(r http.ResponseWriter, req *http.Request) {
+		go chat.Client().Run()
+		r.WriteHeader(http.StatusOK)
+		r.Write([]byte("<p>Chat bot started</p>"))
+		return
+	})
+	mux.Handle("/on", onHandler)
+
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatalln(err)
 	}
